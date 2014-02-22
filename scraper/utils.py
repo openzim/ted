1cbbd367de6b4b0b15b21f774c083464a59a767e
Rashiq Ahmad <rashiq.z@gmail.com>
2014-02-22 22:08:29 +0100

    Working Scraper

--- scraper/utils.py
+++ scraper/utils.py
@@ -12,3 +12,6 @@ __license__ = 'GPLv3'
 
 def create_absolute_link(base, rel_url):
     return urljoin(base, rel_url)
+
+def build_video_page(page):
+    return 'http://new.ted.com/talks/browse?page=' + str(page)
--- scraper/webscraper.py
+++ scraper/webscraper.py
@@ -18,24 +18,34 @@ class Scraper():
     BASE_URL = 'http://new.ted.com/talks/browse'
     soup = None
     pages = None
+    videos = []
 
 
     def __init__(self):
         self.pages = self.extract_page_number()
-        self.extract_videos()
+        self.extract_all_video_links()
         
 
     def extract_page_number(self):
         self.soup = BeautifulSoup(requests.get(self.BASE_URL).text)
-        self.pages = self.soup.select('div.pagination a.pagination__item')[-1]
+        pages = self.soup.select('div.pagination a.pagination__item')[-1]
+        return int(pages.text)
+        
+
+    def extract_all_video_links(self):
+        for page in range(1, self.pages):
+            url = utils.build_video_page(page)
+            html = requests.get(url).text
+            self.soup = BeautifulSoup(html)
+            self.extract_videos()
+            print self.videos
 
 
     def extract_videos(self):
-        videos = []
-
         for video in self.soup.select('div.row div.media__image a'):
-            print utils.create_absolute_link(self.BASE_URL, video['href'])
-            print '\n\n'
+            url = utils.create_absolute_link(self.BASE_URL, video['href'])
+            self.videos.append(url)
+
 
 
 
