f908745273478f188f22d0d5ead1bd43b29fe9f6
Satyam Kumar <io.satyamtg@gmail.com>
2020-07-06 14:48:38 +0530

    Do not display speaker image if it is not present (#92)
    * Fixes #84 - Do not display speaker image if it is not present

--- ted2zim/scraper.py
+++ ted2zim/scraper.py
@@ -520,7 +520,7 @@ class Ted2Zim:
             speaker_info = {
                 "description": "None",
                 "whotheyare": "None",
-                "photo_url": "None",
+                "photo_url": "",
             }
             if "speaker_name" in talk_info:
                 speaker = talk_info["speaker_name"]
@@ -761,7 +761,7 @@ class Ted2Zim:
                     logger.error(f"Could not download {org_video_file_path}")
 
             # download an image of the speaker
-            if video_speaker == "None" or video_speaker == "":
+            if not video_speaker:
                 logger.debug("Speaker doesn't have an image")
             else:
                 logger.debug(f"Downloading Speaker image for {video_title}")
