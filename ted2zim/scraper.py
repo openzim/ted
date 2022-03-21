f87cd8b5ab1151c6c2e0911dd9dd60b0b3f8bb36
renaud gaudin <reg@rskg.org>
2022-03-21 08:43:16 +0000

    dont fail on missing whoTheyAre

--- ted2zim/scraper.py
+++ ted2zim/scraper.py
@@ -567,8 +567,8 @@ class Ted2Zim:
         # Extract the ted talk details from json
         video_id = json_data["id"]
         speaker_profession = speaker_info["description"]
-        speaker_bio = speaker_info["whoTheyAre"]
-        speaker_picture = speaker_info["avatar"]
+        speaker_bio = speaker_info.get("whoTheyAre", "-")
+        speaker_picture = speaker_info.get("avatar", "-")
         title = json_data["title"]
         description = json_data["description"]
         date = dateutil.parser.parse(json_data["recordedOn"]).strftime("%d %B %Y")
